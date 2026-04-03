18dce58d30dd1c59183bdc78c8610bc98f0cfcd7
Beilu521 <383008278@qq.com>
2026-04-03 18:53:38 +0800

    初始化提交，脚手架和部分配置

---
+++ src/test/java/com/deerecho/My_Blog/MyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deerecho.My_Blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
